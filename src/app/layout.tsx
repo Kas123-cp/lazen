3ff93bf253eaecee3c65dbabea99a36f9d01173d
Kas123-cp <kosrmughl313@gmail.com>
2025-08-04 13:19:09 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -104,7 +104,26 @@ export default function RootLayout({
             }),
           }}
         />
+
+        {/* Tawk.to Live Chat Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+              (function() {
+                var s1 = document.createElement("script"),
+                    s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/68906c1b7f808f192df0b098/1j1q2cj70';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin', '*');
+                s0.parentNode.insertBefore(s1, s0);
+              })();
+            `,
+          }}
+        />
       </Head>
+
       <body
         className={cn(
           'min-h-screen bg-background font-body antialiased',
